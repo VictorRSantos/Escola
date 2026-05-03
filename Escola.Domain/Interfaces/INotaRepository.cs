5e2dd918068e07554a11c81fa4a9162202386c2d
Victor R Santos <vrsisa@hotmail.com>
2026-05-03 12:15:05 -0300

    Criando Interfaces repository

---
+++ Escola.Domain/Interfaces/INotaRepository.cs
@@ -0,0 +1,13 @@
+﻿using Escola.Domain.Entities;
+
+namespace Escola.Domain.Interfaces
+{
+    public interface INotaRepository
+    {
+        Task<Nota> GetByIdAsync(int id);
+        Task<List<Nota>> GetAllAsync();
+        Task<Nota> AddAsync(Nota nota);
+        Task<Nota> UpdateAsync(Nota nota);
+        Task<Nota> DeleteAsync(int id);
+    }
+}
